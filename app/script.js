14d81172c62b39d87fb982730662223e21f7557c
Peter Chen <dolphinmail321@gmail.com>
2016-08-06 17:25:54 -0700

    "user can enter marks for a second round"

--- app/script.js
+++ app/script.js
@@ -1,5 +1,5 @@
 var likemarks = [];
-var time;
+// var time;
 
 function addOne() {
  var time = player.getCurrentTime();
@@ -9,17 +9,25 @@ function addOne() {
 }
 
 function playWithFlow() {
+
+  // resetting the displayed marks
+  document.getElementById("marks").innerHTML = '';
+  document.getElementById("marksList").innerHTML = '';
+
+  // resetting the like marks
+  var thisLikeMarks = likemarks.slice(0);
+  likemarks = [];
   player.stopVideo();
   player.playVideo();
+  player.setPlaybackRate(2);
 
-  if (marksList.length = 0) {
+  if (thisLikeMarks.length === 0) {
     alert("You don't have any marks");
   } else {
         var indexCounter = 0;
-
         // set the first time mark
-        var currentMark = likemarks[indexCounter];
-        var nextMark = likemarks[indexCounter + 1];
+        var currentMark = thisLikeMarks[indexCounter];
+        var nextMark = thisLikeMarks[indexCounter + 1];
         var isSlowed = false;
 
         setInterval(function(){
@@ -27,8 +35,8 @@ function playWithFlow() {
 
                         if (currentTime > nextMark) {
                             indexCounter = indexCounter + 1;
-                            currentMark = likemarks[indexCounter]; // moves on to the next time mark
-                            nextMark = likemarks[indexCounter + 1];
+                            currentMark = thisLikeMarks[indexCounter]; // moves on to the next time mark
+                            nextMark = thisLikeMarks[indexCounter + 1];
                         }
 
                         // setting to X1
@@ -42,14 +50,12 @@ function playWithFlow() {
                             console.log("set to x2 at: "+currentTime);
                             player.setPlaybackRate(2);
                             indexCounter = indexCounter + 1;
-                            currentMark = likemarks[indexCounter]; // moves on to the next time mark
-                            nextMark = likemarks[indexCounter + 1];
+                            currentMark = thisLikeMarks[indexCounter]; // moves on to the next time mark
+                            nextMark = thisLikeMarks[indexCounter + 1];
                             isSlowed = false;
                         }
                       },200);
-
     }
-
 }
 
 $(window).load(function(){
@@ -61,20 +67,18 @@ $(window).load(function(){
           'autoplay': 1
         },
         events: {
-          'onReady': onPlayerReady
+         // 'onReady': onPlayerReady
         }
       });
     }
 
-    function onPlayerReady(event) {
-     event.target.setPlaybackRate(2);
-    }
+//    function onPlayerReady(event) {
+//     event.target.setPlaybackRate(2);
+//    }
 
     $(function() {
 
       embedYouTubeVideo();
 
-
     });
-
 });
